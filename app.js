d4930ff8af5af85629397ced38f1b55e484163e7
markusama133-lang <markusama133@gmail.com>
2025-12-11 00:31:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const API_URL = '';
+    const API_URL = 'http://127.0.0.1:5000';
 
     // --- عناصر الواجهة (DOM Elements) ---
     const phoneGrid = document.getElementById('phone-grid');
